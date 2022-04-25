8d4c60688ab4e3fa7dbb2a657025e127f4be7f81
polodumbo <104343387+polodumbo@users.noreply.github.com>
2022-04-25 15:44:32 +0900

    Add files via upload

---
+++ day02/src/day02/PrintTest.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		// 주석
+		// 1. 소스코드에 설명글을 달 때
+		// 2. 지금 당장 사용하지 않는 코드를 지우고 싶지 않을 때
+		
+		// 주석의 종류
+		/*
+		 * 1. 한 줄 주석 : Ctrl + \
+		 * 2. 범위 주석 : Ctrl + Shift + \, 범위 주석 해제 : Ctrl + Shift + \
+		*/
+		
+		// 이름을 출력하는 부분
+		System.out.print("polodumbo");
+		// 나이를 출력하는 부분
+		System.out.print("20살");
+	}
+}
