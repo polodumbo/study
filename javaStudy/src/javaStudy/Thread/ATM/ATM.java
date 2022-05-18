6fb014c01cdd1121680ec5fe0bfa64f634b8502c
polodumbo <104343387+polodumbo@users.noreply.github.com>
2022-05-18 18:23:03 +0900

    Add files via upload

---
+++ javaStudy/src/javaStudy/Thread/ATM/ATM.java
@@ -0,0 +1,20 @@
+package javaStudy.Thread.ATM;
+
+public class ATM implements Runnable {
+	int money = 10000;
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			withdraw(1000);
+			try {Thread.sleep(500);} catch (Exception e) {;}
+		}
+	}
+	
+	public synchronized void withdraw(int money) {
+		/* synchronized (this) {*/this.money -= money;/*} */
+		System.out.println(Thread.currentThread().getName() + "(이)가 " + money + "원 출금");
+		System.out.println("현재 잔액 : " + this.money + "원");
+	}
+	
+}
